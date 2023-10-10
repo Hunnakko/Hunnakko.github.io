07265525bae43f1d1eeaade5aa180e0670626193
Hunnakko <147532992+Hunnakko@users.noreply.github.com>
2023-10-10 22:13:55 +0300

    Add files via upload

---
+++ src/views/antti-hunnakko.js
@@ -0,0 +1,243 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './antti-hunnakko.css'
+
+const AnttiHunnakko = (props) => {
+  return (
+    <div className="antti-hunnakko-container">
+      <Helmet>
+        <title>Tinted Tinted Goldfinch</title>
+        <meta property="og:title" content="Tinted Tinted Goldfinch" />
+      </Helmet>
+      <div className="antti-hunnakko-container1">
+        <div className="antti-hunnakko-container2">
+          <div className="antti-hunnakko-container3">
+            <h1 className="antti-hunnakko-text">Antti Hunnakko</h1>
+            <span className="antti-hunnakko-text01">
+              Electrical engineering, industrial automation and other projects
+            </span>
+          </div>
+        </div>
+        <div className="antti-hunnakko-feature-card">
+          <svg viewBox="0 0 1024 1024" className="antti-hunnakko-icon">
+            <path d="M809.003 291.328l-297.003 171.819-297.003-171.819 275.456-157.397c4.779-2.731 9.899-4.48 15.147-5.333 9.301-1.451 18.987 0.128 27.904 5.291zM491.776 979.669c6.016 3.243 12.928 5.077 20.224 5.077 7.381 0 14.336-1.877 20.395-5.163 15.189-2.475 29.909-7.68 43.392-15.36l298.709-170.709c26.368-15.232 45.269-38.315 55.424-64.597 5.675-14.592 8.619-30.165 8.747-46.251v-341.333c0-20.395-4.821-39.723-13.397-56.917-0.939-3.029-2.219-5.973-3.883-8.832-1.963-3.371-4.267-6.357-6.912-8.96-1.323-1.835-2.731-3.669-4.139-5.419-9.813-12.203-21.845-22.528-35.456-30.507l-299.051-170.88c-26.027-15.019-55.467-19.84-83.328-15.531-15.531 2.432-30.507 7.637-44.288 15.488l-298.709 170.709c-16.341 9.429-29.824 21.888-40.149 36.267-2.56 2.56-4.864 5.547-6.784 8.832-1.664 2.901-2.987 5.888-3.925 8.96-1.707 3.456-3.243 6.955-4.608 10.496-5.632 14.635-8.576 30.208-8.704 45.995v341.632c0.043 30.293 10.581 58.197 28.331 80.128 9.813 12.203 21.845 22.528 35.456 30.507l299.051 170.88c13.824 7.979 28.587 13.099 43.605 15.445zM469.333 537.045v340.949l-277.12-158.336c-4.736-2.773-8.832-6.315-12.16-10.411-5.931-7.381-9.387-16.512-9.387-26.581v-318.379zM554.667 877.995v-340.949l298.667-172.757v318.379c-0.043 5.163-1.067 10.496-2.987 15.445-3.413 8.789-9.6 16.384-18.176 21.333z"></path>
+          </svg>
+          <h2 className="antti-hunnakko-text02">Who am I?</h2>
+          <span className="antti-hunnakko-text03">
+            <span className="antti-hunnakko-text04">
+              Hello! My name is Antti Hunnakko, and I am an automation engineer
+              from Seinäjoki.
+            </span>
+            <br className="antti-hunnakko-text05"></br>
+            <span className="antti-hunnakko-text06">
+              I was born in 1994, and when it came a time for me to pick a
+              profession, I chose an electrician and worked as one for a year.
+            </span>
+            <br className="antti-hunnakko-text07"></br>
+            <span className="antti-hunnakko-text08">
+              After that year working as an electrician was up, I got into
+              Seinäjoki University of Applied Sciences, where I studied to
+              become an automation engineer.
+            </span>
+            <br className="antti-hunnakko-text09"></br>
+            <span className="antti-hunnakko-text10">
+              During my studies I started an internship at a company where I
+              first did mechanical engineering, and later transitioned into
+              electrical engieering.
+            </span>
+            <br className="antti-hunnakko-text11"></br>
+            <span className="antti-hunnakko-text12">
+              After my internship was over, I stayed with the company for 3
+              years before going back to complete my bachelors degree.
+            </span>
+            <br className="antti-hunnakko-text13"></br>
+            <span className="antti-hunnakko-text14">
+              I finally officially got my bachelors degree in automation
+              engineering in 2022, and finished my studies with 3 years of work
+              experience in my field!
+            </span>
+            <br></br>
+          </span>
+        </div>
+        <div className="antti-hunnakko-feature-card1">
+          <svg viewBox="0 0 1024 1024" className="antti-hunnakko-icon02">
+            <path d="M809.003 291.328l-297.003 171.819-297.003-171.819 275.456-157.397c4.779-2.731 9.899-4.48 15.147-5.333 9.301-1.451 18.987 0.128 27.904 5.291zM491.776 979.669c6.016 3.243 12.928 5.077 20.224 5.077 7.381 0 14.336-1.877 20.395-5.163 15.189-2.475 29.909-7.68 43.392-15.36l298.709-170.709c26.368-15.232 45.269-38.315 55.424-64.597 5.675-14.592 8.619-30.165 8.747-46.251v-341.333c0-20.395-4.821-39.723-13.397-56.917-0.939-3.029-2.219-5.973-3.883-8.832-1.963-3.371-4.267-6.357-6.912-8.96-1.323-1.835-2.731-3.669-4.139-5.419-9.813-12.203-21.845-22.528-35.456-30.507l-299.051-170.88c-26.027-15.019-55.467-19.84-83.328-15.531-15.531 2.432-30.507 7.637-44.288 15.488l-298.709 170.709c-16.341 9.429-29.824 21.888-40.149 36.267-2.56 2.56-4.864 5.547-6.784 8.832-1.664 2.901-2.987 5.888-3.925 8.96-1.707 3.456-3.243 6.955-4.608 10.496-5.632 14.635-8.576 30.208-8.704 45.995v341.632c0.043 30.293 10.581 58.197 28.331 80.128 9.813 12.203 21.845 22.528 35.456 30.507l299.051 170.88c13.824 7.979 28.587 13.099 43.605 15.445zM469.333 537.045v340.949l-277.12-158.336c-4.736-2.773-8.832-6.315-12.16-10.411-5.931-7.381-9.387-16.512-9.387-26.581v-318.379zM554.667 877.995v-340.949l298.667-172.757v318.379c-0.043 5.163-1.067 10.496-2.987 15.445-3.413 8.789-9.6 16.384-18.176 21.333z"></path>
+          </svg>
+          <h2 className="antti-hunnakko-text16">What do I do for a living?</h2>
+          <span className="antti-hunnakko-text17">
+            <span>
+              My professional career has focused on automation and electrical
+              engineering in an industrial setting.
+            </span>
+            <br></br>
+            <span>
+              Main part of my career has been designing all things electricity
+              and automation for my employers projects.
+            </span>
+            <br></br>
+            <span>
+              Everything from designing and drawing the electrical schematics
+              and cabinet layouts, selecting the necessary PLC components for
+              the project, writing the
+            </span>
+            <br></br>
+            <span>
+              process model of the automation system for the PLC programmer to
+              make code from, writing the manuals for the end users to use our
+              systems, and being on
+            </span>
+            <br></br>
+            <span>site during the commissioning of our projects.</span>
+          </span>
+        </div>
+        <div className="antti-hunnakko-container4">
+          <div className="antti-hunnakko-feature-card2">
+            <svg viewBox="0 0 1024 1024" className="antti-hunnakko-icon04">
+              <path d="M809.003 291.328l-297.003 171.819-297.003-171.819 275.456-157.397c4.779-2.731 9.899-4.48 15.147-5.333 9.301-1.451 18.987 0.128 27.904 5.291zM491.776 979.669c6.016 3.243 12.928 5.077 20.224 5.077 7.381 0 14.336-1.877 20.395-5.163 15.189-2.475 29.909-7.68 43.392-15.36l298.709-170.709c26.368-15.232 45.269-38.315 55.424-64.597 5.675-14.592 8.619-30.165 8.747-46.251v-341.333c0-20.395-4.821-39.723-13.397-56.917-0.939-3.029-2.219-5.973-3.883-8.832-1.963-3.371-4.267-6.357-6.912-8.96-1.323-1.835-2.731-3.669-4.139-5.419-9.813-12.203-21.845-22.528-35.456-30.507l-299.051-170.88c-26.027-15.019-55.467-19.84-83.328-15.531-15.531 2.432-30.507 7.637-44.288 15.488l-298.709 170.709c-16.341 9.429-29.824 21.888-40.149 36.267-2.56 2.56-4.864 5.547-6.784 8.832-1.664 2.901-2.987 5.888-3.925 8.96-1.707 3.456-3.243 6.955-4.608 10.496-5.632 14.635-8.576 30.208-8.704 45.995v341.632c0.043 30.293 10.581 58.197 28.331 80.128 9.813 12.203 21.845 22.528 35.456 30.507l299.051 170.88c13.824 7.979 28.587 13.099 43.605 15.445zM469.333 537.045v340.949l-277.12-158.336c-4.736-2.773-8.832-6.315-12.16-10.411-5.931-7.381-9.387-16.512-9.387-26.581v-318.379zM554.667 877.995v-340.949l298.667-172.757v318.379c-0.043 5.163-1.067 10.496-2.987 15.445-3.413 8.789-9.6 16.384-18.176 21.333z"></path>
+            </svg>
+            <h2 className="antti-hunnakko-text27">
+              What systems and tools am I familiar with?
+            </h2>
+            <span className="antti-hunnakko-text28">
+              <span>
+                For electrical drawings I am most familiar with CADMATIC
+                Electrical, and I have designed with it for 4 years.
+              </span>
+              <br></br>
+              <span>
+                From the PLC world I am most familiar with Beckhoff products and
+                TwinCAT, but I have also done projects with Siemens and TIA
+                Portal 
+              </span>
+              <br></br>
+              <span>
+                and some small test projects with Schneider PLC&apos;s and
+                Machine Expert.
+              </span>
+              <br></br>
+              <span>
+                Even with some experience in PLC programming I am not a PLC
+                programmer. I can manage small projects, but whole factories are
+                out 
+              </span>
+              <br></br>
+              <span>
+                of my comfort zone when it comes to programming. Programming
+                PLC&apos;s is something I would like to do more and get better
+                at.
+              </span>
+              <br></br>
+              <br></br>
+              <span>
+                Besides the before mentioned career specific tools, I am also
+                tech savy and know my way around Windows and Microsoft products,
+              </span>
+              <br></br>
+              <span>
+                and can usually overcome and find solutions to obstacles
+                regarding technology.
+              </span>
+              <br></br>
+              <br></br>
+              <span>
+                From programming languages I am most proficient in C#, but have
+                done coding in C++ before with success.
+              </span>
+              <br></br>
+              <br></br>
+            </span>
+          </div>
+        </div>
+        <div className="antti-hunnakko-feature-card3">
+          <svg viewBox="0 0 1024 1024" className="antti-hunnakko-icon06">
+            <path d="M809.003 291.328l-297.003 171.819-297.003-171.819 275.456-157.397c4.779-2.731 9.899-4.48 15.147-5.333 9.301-1.451 18.987 0.128 27.904 5.291zM491.776 979.669c6.016 3.243 12.928 5.077 20.224 5.077 7.381 0 14.336-1.877 20.395-5.163 15.189-2.475 29.909-7.68 43.392-15.36l298.709-170.709c26.368-15.232 45.269-38.315 55.424-64.597 5.675-14.592 8.619-30.165 8.747-46.251v-341.333c0-20.395-4.821-39.723-13.397-56.917-0.939-3.029-2.219-5.973-3.883-8.832-1.963-3.371-4.267-6.357-6.912-8.96-1.323-1.835-2.731-3.669-4.139-5.419-9.813-12.203-21.845-22.528-35.456-30.507l-299.051-170.88c-26.027-15.019-55.467-19.84-83.328-15.531-15.531 2.432-30.507 7.637-44.288 15.488l-298.709 170.709c-16.341 9.429-29.824 21.888-40.149 36.267-2.56 2.56-4.864 5.547-6.784 8.832-1.664 2.901-2.987 5.888-3.925 8.96-1.707 3.456-3.243 6.955-4.608 10.496-5.632 14.635-8.576 30.208-8.704 45.995v341.632c0.043 30.293 10.581 58.197 28.331 80.128 9.813 12.203 21.845 22.528 35.456 30.507l299.051 170.88c13.824 7.979 28.587 13.099 43.605 15.445zM469.333 537.045v340.949l-277.12-158.336c-4.736-2.773-8.832-6.315-12.16-10.411-5.931-7.381-9.387-16.512-9.387-26.581v-318.379zM554.667 877.995v-340.949l298.667-172.757v318.379c-0.043 5.163-1.067 10.496-2.987 15.445-3.413 8.789-9.6 16.384-18.176 21.333z"></path>
+          </svg>
+          <h2 className="antti-hunnakko-text48">What else ?</h2>
+          <span className="antti-hunnakko-text49">
+            <span>I do a lot!</span>
+            <br></br>
+            <br></br>
+            <span>
+              There are many things that I find interesting and usually spend my
+              free time studying, doing test projects or just trying to
+              understand a new topic.
+            </span>
+            <br></br>
+            <span>
+              I spend a lot of my free time in front of my computer, and besides
+              my career, I am interested in technology, video games and
+              computing.
+            </span>
+            <br></br>
+            <span>
+              When I need to upgrade my PC, I do it myself. I have build my own
+              PC&apos;s for the past 15 years.
+            </span>
+            <br></br>
+            <br></br>
+            <span>
+              I do a lot of gaming on PC, and through that I have started to
+              develop modifications to games I like to improve or add systems to
+              them.
+            </span>
+            <br></br>
+            <span>
+              Developing modifications for games has been a great boost to my
+              programming abilities in C# and C++.
+            </span>
+            <br></br>
+            <span>
+              I have also done some protorype games in Unreal Engine 4/5 and in
+              Unity game engines.
+            </span>
+            <br></br>
+            <br></br>
+            <span>
+              I also own a 3D printer and I design most models I print myself on
+              Autodesk Fusion 360.
+            </span>
+            <br></br>
+            <span>
+              I have also done simple test projects with an Arduino and ESP32
+              developement boards.
+            </span>
+            <br></br>
+            <span>
+              Electronics and embedded systems are not my expertise, but they
+              are something I want to get better at.
+            </span>
+            <br></br>
+            <br></br>
+            <span>
+              Besides all that more technical stuff, I listen to audiobooks and
+              paint warhammer miniatures!
+            </span>
+            <br></br>
+          </span>
+          <span className="antti-hunnakko-text76">
+            SEE MORE INFORMATION ABOUT MY PROJECTS
+          </span>
+        </div>
+      </div>
+      <div className="antti-hunnakko-social-bar">
+        <div className="antti-hunnakko-container5">
+          <span className="antti-hunnakko-text77">
+            <span>Check out my github - </span>
+            <br></br>
+          </span>
+          <svg
+            viewBox="0 0 877.7142857142857 1024"
+            className="antti-hunnakko-icon08"
+          >
+            <path d="M296.571 685.714v0c1.143-1.714 0.571-5.143-1.714-7.429-2.857-2.286-6.286-2.857-8-1.143-1.143 1.714-0.571 5.143 1.714 7.429 2.857 2.286 6.286 2.857 8 1.143zM280.571 662.286c-1.714-2.286-4.571-3.429-6.857-2.286-1.714 1.143-1.714 4.571 0 6.857 2.286 2.857 5.143 4 6.857 2.857v0c1.714-1.143 1.714-4.571 0-7.429zM257.143 639.429v0c0.571-1.143-0.571-3.429-2.857-4.571-1.714-0.571-4-0.571-4.571 1.143-1.143 1.714 0 3.429 2.286 4.571 2.286 0.571 4.571 0.571 5.143-1.143zM269.143 652.571v0c1.143-1.143 1.143-4-1.143-5.714-1.714-2.286-4.571-2.857-5.714-1.714-1.714 1.714-1.143 4 0.571 6.286 1.714 1.714 4.571 2.857 6.286 1.143zM318.286 695.429v0c0.571-2.286-1.714-5.143-5.143-6.286s-6.286 0-7.429 2.286c-0.571 2.286 1.714 5.143 5.143 6.286s6.286 0 7.429-2.286zM342.286 697.143v0c0-2.286-2.857-4.571-6.857-4.571-3.429 0-5.714 2.286-5.714 4.571s2.857 4.571 6.286 4.571 6.286-2.286 6.286-4.571zM364.571 693.143v0c-0.571-2.286-4-3.429-7.429-2.857s-5.714 2.857-5.143 5.143 3.429 4 6.857 3.429 5.714-3.429 5.714-5.714zM731.429 512c0-161.714-130.857-292.571-292.571-292.571s-292.571 130.857-292.571 292.571c0 129.143 84 238.857 200 277.714 14.857 2.857 20-6.286 20-14.286 0-6.857 0-29.714-0.571-54.286 0 0-81.143 17.714-98.286-34.857 0 0-13.143-33.714-32.571-42.286 0 0-26.286-18.286 2.286-18.286 0 0 28.571 2.286 44.571 30.286 25.714 45.143 68.571 32 85.143 24.571 2.857-18.857 10.286-32 18.857-39.429-65.143-7.429-133.714-32.571-133.714-144.571 0-32 11.429-57.714 30.286-78.286-2.857-7.429-13.143-37.143 2.857-77.714 24.571-7.429 80.571 30.286 80.571 30.286 23.429-6.857 48-9.714 73.143-9.714s49.714 2.857 73.143 9.714c0 0 56-37.714 80.571-30.286 16 40.571 5.714 70.286 2.857 77.714 18.857 20.571 30.286 46.286 30.286 78.286 0 112.571-68.571 137.143-133.714 144.571 10.286 9.143 20 26.857 20 54.286 0 38.857-0.571 70.286-0.571 80 0 8 5.143 17.143 20 14.286 116-38.857 200-148.571 200-277.714zM877.714 237.714v548.571c0 90.857-73.714 164.571-164.571 164.571h-548.571c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571c90.857 0 164.571 73.714 164.571 164.571z"></path>
+          </svg>
+        </div>
+        <span className="antti-hunnakko-text80">Antti Hunnakko 2023</span>
+      </div>
+    </div>
+  )
+}
+
+export default AnttiHunnakko
